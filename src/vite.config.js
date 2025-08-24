863a75c28980e091e0abd5714a0b5601cca5e81b
Paul Serrano <paul.serrano08374@gmail.com>
2025-08-24 21:11:03 +0200

    build(Docker,-Laravel): install docker and laravel inside the container

---
+++ src/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0', // expose à tout le réseau
+        port: 5173,
+        hmr: {
+            host: 'localhost', // important pour Docker + navigateur host
+        },
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
